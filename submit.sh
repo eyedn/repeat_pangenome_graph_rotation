6567ad15d5a47d270b7497dd3c290571f8d5a337
Aydin Karatas <alckaratas18@gmail.com>
2024-09-18 00:19:11 -0700

    revert with new test var

--- submit.sh
+++ submit.sh
@@ -11,21 +11,37 @@
 #           submit.sh
 ###############################################################################
 
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem=16000
+#SBATCH --partition=qcb
+#SBATCH --account=mchaisso_100
+#SBATCH -N 1
+#SBATCH --job-name=rpggaln
+#SBATCH --output=slurm.%A_%a.%x.log 
+###SBATCH --constraint=xeon-2665,avx
+###SBATCH --exclude=b10-10
+###SBATCH --mail-type=ALL
+###SBATCH --mail-user=karatas@usc.edu
 
-genomes=$(wc -l < /scratch1/tsungyul/n30488.hprc.full/1kg/genomes.txt) 
+source ~/.bashrc
+set -eu
+module load gcc/11.3.0 #usc samtools
 
-sbatch \
-	--ntasks=1 \
-	--time=48:00:00 \
-	--mem=16000 \
-	--partition=qcb \
-	--account=mchaisso_100 \
-	-N 1 \
-	--job-name=rpggaln \
-	--output=slurm.%A_%a.%x.log \
-	--constraint=xeon-2665,avx \
-	--exclude=b10-10 \
-	--mail-type=ALL \
-	--mail-user=karatas@usc.edu \
-	--array=0-1 \
-	./align.sh 
+
+date
+# echo "task no.: $SLURM_ARRAY_TASK_ID"
+# g=$(head -n $SLURM_ARRAY_TASK_ID /scratch1/tsungyul/n30488.hprc.full/1kg/genomes.txt | \
+# 		tail -n 1)
+
+g=$(head -n 1 /scratch1/tsungyul/n30488.hprc.full/1kg/genomes.txt | \
+		tail -n 1)
+echo "genome: $g"
+
+rpgg=/scratch1/tsungyul/aydin/input/pan
+fa=/scratch1/tsungyul/n30488.hprc.full/1kg/varcall1/fa/$g.fa
+out=/scratch1/tsungyul/aydin/output/$g.aln.gz
+
+/project/mchaisso_100/cmb-16/tsungyul/work/vntr/danbing-tk/bin/microdanbing -k 21 -qs $rpgg -f $fa | gzip >$out
+
+date
