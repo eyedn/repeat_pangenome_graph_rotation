f83ab8d38c3abdb4562520e4cab2503256f61d5b
Aydin Karatas <alckaratas18@gmail.com>
2024-09-21 17:59:02 -0700

    renamed shell files

--- get_invar_count.sh
+++ get_invar_count.sh
@@ -8,19 +8,20 @@
 #           Department of Quantitative and Computational Biology 
 #           Chaisson Lab Rotation
 #           ---
-#           get_bubble_alignments.sh
+#           get_invar_count.sh
 ###############################################################################
 
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
-#SBATCH --mem=16000
+#SBATCH --mem=4000
 #SBATCH --partition=chaissonlab
 #SBATCH --account=mchaisso_100
 #SBATCH -N 1
-#SBATCH --job-name=cigar
-#SBATCH --output=/project/mchaisso_100/cmb-17/vntr_genotyping/aydin/log/slurm.%A_%a.%x.log
+#SBATCH --job-name=rpggaln
+#SBATCH --output=/project/mchaisso_100/cmb-17/vntr_genotyping/aydin/log/slurm.%A_%a.%x.log 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=karatas@usc.edu
+
 
 source ~/.bashrc
 module load conda
@@ -37,11 +38,10 @@ g=$(head -n $SLURM_ARRAY_TASK_ID /scratch1/tsungyul/n30488.hprc.full/1kg/genomes
 echo "genome: $g"
 
 rpgg=/project/mchaisso_100/cmb-17/vntr_genotyping/aydin/input/pan
-aln=/scratch1/tsungyul/aydin/output/$g.aln.gz
-out=/scratch1/tsungyul/aydin/output/$g.csv
+fa=/scratch1/tsungyul/n30488.hprc.full/1kg/varcall1/fa/$g.fa
+out=/scratch1/tsungyul/aydin/output/$g.pickle
 
-
-python3 ./get_bubble_alignments.py $g $aln $out
+python3 ./get_invar_count.py $g $fa $out
 
 echo "all done!"
 date
