f8044a1e8b9ce6dfc9891dc22ea1184089bed4af
Aydin Karatas <alckaratas18@gmail.com>
2024-10-20 18:53:10 -0700

    created separate script for running on carc + exe. permission for all *sh

--- submit_2.sh
+++ submit_2.sh
@@ -8,12 +8,8 @@
 #           Department of Quantitative and Computational Biology 
 #           Chaisson Lab Rotation
 #           ---
-#           2_1_cat_aln_csv.sh
+#           submit_2.sh
 ###############################################################################
 
 
-csv_dir="/scratch1/tsungyul/aydin/output"
-file_1="$csv_dir/HG00096.csv"
-out="/project/mchaisso_100/cmb-17/vntr_genotyping/aydin/data/cigar_data.csv"
-
-head -n 1 $file_1 > $out && tail -n +2 -q $csv_dir/*.csv >> $out
+sbatch 2_get_motif_data.sh
