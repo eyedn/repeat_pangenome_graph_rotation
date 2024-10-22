57f4511cb409c6d9bbae4d300bc9160accb9d8fc
Aydin Karatas <alckaratas18@gmail.com>
2024-10-21 17:07:22 -0700

    added extra print statements

--- python_scripts/convert_kmers_to_motifs.py
+++ python_scripts/convert_kmers_to_motifs.py
@@ -10,11 +10,12 @@
 ###############################################################################
 
 
-from sys import argv
+import sys
 import glob
 import os
 import numpy as np
 import pickle
+import time 
 
 
 def get_1(file_path):
@@ -41,34 +42,48 @@ def compute_gt_cgt_single_batch(batch_num, kmer_dir, out_dir, ki_map, tr_cck_ns,
     
     BS = ng // NB
     BS_ = BS if batch_num != NB-1 else ng - BS*batch_num
+    print(f"BS: {BS}", end="")
+    print(f"BS_: {BS_}", end="")
+    sys.stdout.flush()
 
     print(f"batch {batch_num}: loading gt... ", end="")
+    sys.stdout.flush()
+    start_time = time.time()
     gt = np.zeros([NK, BS_], dtype=np.int32)
     for j in range(BS_):
         load_single_gt(kmerfs[batch_num*BS+j], gt, j)
-    
+    load_time = time.time() - start_time
+    print(f"Loading gt for batch {batch_num} took {load_time:.2f} seconds")
+    sys.stdout.flush()
+
     print(f"computing cgt... ", end="")
+    sys.stdout.flush()
+    start_time = time.time()
     cgt = np.zeros([NCCK, BS_], dtype=np.float32)
     for i0, i1 in ki_map.items():
         cgt[i1] += gt[i0]
-    
+    compute_time = time.time() - start_time
     cgt /= tr_cck_ns[:, None]
+    print(f"Computing cgt for batch {batch_num} took {compute_time:.2f} seconds")
+    sys.stdout.flush()
 
     print("dumping gt... ", end="")
+    sys.stdout.flush()
     with open(f"{out_dir}/gt.{batch_num}.pickle", 'wb') as f:
         pickle.dump(gt, f, protocol=pickle.HIGHEST_PROTOCOL)
     
     print("dumping cgt...")
+    sys.stdout.flush()
     with open(f"{out_dir}/cgt.{batch_num}.pickle", 'wb') as f:
         pickle.dump(cgt, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == "__main__":
-    get_1_file = argv[1]
-    get_2_file = argv[2]
-    batch_num = int(argv[3]) 
-    total_batches = int(argv[4])
-    kmers = argv[5]
-    out = argv[6]
+    get_1_file = sys.argv[1]
+    get_2_file = sys.argv[2]
+    batch_num = int(sys.argv[3]) 
+    total_batches = int(sys.argv[4])
+    kmers = sys.argv[5]
+    out = sys.argv[6]
     
     if not os.path.exists(out):
         os.makedirs(out)
